330c4ecd3e289cecb2f64fa5f7b5134fc7113510
hactar <hactar@cyberkraft.ch>
2020-03-31 10:57:55 +0200

    Remove transition because of bugs

--- alpinejs_stundenplan/postcss.config.js
+++ alpinejs_stundenplan/postcss.config.js
@@ -2,7 +2,7 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: [
-    './index.html',
+    'public/index.html',
   ],
 
   // Include any special characters you're using in this regular expression
